b76f884c0ea840ef49884774c453d0013eb9aff2
Isak2003 <70603096+snurkeburk@users.noreply.github.com>
2020-12-13 04:57:04 +0100

    fixed pos and responsiveness on signup text and button on login page, fixed color alpha on main page, added comments on js for future, added :hover for buttons (cursor pointer, increased alpha on hover)

--- loginScript.js
+++ loginScript.js
@@ -6,7 +6,7 @@ var k;
 function check(){
 input_fields.forEach(function(input_item){
     input_item.addEventListener("input",function(){
-        if(input_item.value == "dev"){
+        if(input_item.value == "dev"){ // här kan man ändra till en variabel istället för "dev"
             console.log(input_item.value);
             k = true;     
             localStorage.setItem("trueORFalse ", k);         
@@ -16,7 +16,7 @@ input_fields.forEach(function(input_item){
     var input_fields_password = document.querySelectorAll(".password");
         input_fields_password.forEach(function(input_item_password){
         input_item_password.addEventListener("input",function(){
-            if(input_item_password.value == "123"){
+            if(input_item_password.value == "123"){ // här kan man ha en PW variabel istället för "123"
                 console.log(input_item_password.value);
                 var j = true;
             } else {
