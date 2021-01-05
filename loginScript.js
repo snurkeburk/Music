c25963789d8aefa2ac8ff816d8448ba04b3b0679
Isak2003 <70603096+snurkeburk@users.noreply.github.com>
2021-01-06 00:23:29 +0100

    fixed login and signup buttons so they go away when user is logged in. Styled buttons

--- loginScript.js
+++ loginScript.js
@@ -9,8 +9,7 @@ function putCookie(form)
   {
    setCookie("userName", form[0].usrname.value);
    setCookie("passWord", form[0].psword.value);
-
-   alert(document.cookie);
+   window.location.href = "/index.html";
    return true;
 }
 
@@ -28,35 +27,89 @@ function putCookie(form)
     }
 
 //------------ Login -----------//
-
-
-function logIn() {
-    var a = getCookie("userName");
-    var b = getCookie("passWord");
-    var loggedIn = false;
-    var inputVal = document.querySelector(".username").value;
-    var inputValPw= document.querySelector(".password").value;
-    var j = document.querySelectorAll("logout");
+function hide(){
+    loggedIn = false;
     var k = document.getElementById("login");
     var l = document.getElementById("signup");
+    var icon = document.getElementById("account");
+    var name = document.getElementById("name");
+    console.log("hide");
+    if(loggedIn == false){
+    l.style.display = "block";
+    k.style.display = "block";
+    icon.style.display = "none";
+    name.style.display = "none";
+    console.log("loggedIn == false");
+} else if (loggenId == true){
+    l.style.display = "none";
+    k.style.display = "none";
+    icon.style.display = "block";
+    name.style.display = "block";
+}
+
+    
+}
+
+
+function logIn() {   
+    var a = getCookie("userName");
+    var b = getCookie("passWord");
+    var k = document.getElementById("login");
+    var l = document.getElementById("signup");
+    var icon = document.getElementById("account");
+    var name = document.getElementById("name");
+    var loggedIn;
+    var j = document.querySelectorAll("logout");
     var pw = document.querySelector("password");
+
+    
+    var inputVal = document.querySelector(".username").value;
+    var inputValPw= document.querySelector(".password").value;
     var x = document.forms["myForm"]["fname"].value;
     if(inputVal==a && inputValPw == b){
     console.log("login successful");
     loggedIn = true;
+    name.value = document.querySelector(".username").value;
     window.location.href = "/index.html";
-    k.style.display = "none";
-    l.style.dispaly = "none";
+
    // confirm();
 } else {
     console.log("username / password incorrect!")
     loggedIn = false;
+
+}
+if(loggedIn == false){
+    l.style.display = "block";
     k.style.display = "block";
-    l.style.dispaly = "block";
-    }
+    icon.style.display = "none";
+    name.style.display = "none";
+    console.log("loggedIn == false");
+} else if (loggenId == true){
+    l.style.display = "none";
+    k.style.display = "none";
+    icon.style.display = "block";
+    name.style.display = "block";
 }
 
 
+
+}
+
+function notLoggedIn(){
+
+    loggedIn = false;
+    console.log("not logged in");
+    if(loggedIn == false){
+        k.style.display = "block";
+        l.style.display = "block";
+        icon.style.display = "none";
+        name.style.display = "none";
+        console.log("loggedIn == false");
+        window.location.href = "/index.html";
+    } else{
+        return true;
+    }
+}
 
 
 /*
@@ -72,10 +125,20 @@ function confirm() {
 }
 
 function notLoggedIn(){
-    window.location.href = "/index.html";
-    var j = document.getElementById("accessAccount");
-    var t = document.getElementsById("logid");
-    j.style.display = "block";
-    j.style.display = "block";
-    
+    var ka = document.getElementById("login");
+    var lb = document.getElementById("signup");
+    var iconc = document.getElementById("account");
+    var named = document.getElementById("name");
+    loggedIne = false;
+    console.log("nog logged in");
+    if(loggedIne == false){
+        ka.style.display = "block";
+        lb.style.display = "block";
+        iconc.style.display = "none";
+        named.style.display = "none";
+        console.log("loggedIn == false");
+        window.location.href = "/index.html";
+    } else{
+        return true;
+    }
 }*/
